a61fbb8a06976e04902875d605c0da676b27053c
Kushani <kushanis@cbctechsol.com>
2025-04-28 17:05:40 +0530

    added brutalism styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>hello</Button>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Button variant={"elevated"}>hello</Button>
+      <Input placeholder="input" />
+      <Progress value={50} />
+      <Textarea placeholder="teaaxt" />
     </div>
   );
 }
